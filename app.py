84850e5bdaf9a90b6fa1d5616a6c278ed9ff84f8
paul duncan <smasher1428@gmail.com>
2023-01-27 16:11:05 -0500

    intial commit

--- app.py
+++ app.py
@@ -9,4 +9,30 @@ def parallel(resistor_list):
     print(Total_Resistance, "ohms")
     return Total_Resistance
 
-    print(summation())
+
+
+parallel([300,2000,1000])
+
+def paralell(list):
+    y= 0
+    for x in list:
+        y=y+ 1/y
+
+        y = 1/y
+
+        print(y,"ohms")
+        return y 
+    
+
+
+    value = []
+    def potential_divider(volt,list):
+        total_resistance = sum(list) 
+        
+
+        for x in list: 
+            output_voltage= (volt*x)/total_resistance
+            value.append(output_voltage)
+            return value
+    
+    print(potential_divider(9,[3000,1000]))
---
+++ readme.md
@@ -0,0 +1,6 @@
+The code is epected to us the voltage divider formula to find the total resistor value of any range.
+
+
+The reason for the assignment is to expose student to used Git, GitHub and Python and the development environment.
+
+I "GIT" son knowledge but the "HUB" was missing.
